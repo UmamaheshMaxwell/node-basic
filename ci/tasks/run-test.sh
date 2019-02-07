9a26cd393288265b0588680eb8fc9b74f8e16b8c
Umamaheswararao Meka <uma@olivetech.com>
2019-02-07 12:34:16 +0530

    modified shell file

--- ci/tasks/run-test.sh
+++ ci/tasks/run-test.sh
@@ -2,6 +2,6 @@
 set -e
 
 export NODEPATH=$(pwd)/nodepath:$(pwd)/nodepath/src/github.com/UmamaheshMaxwell/node-basic/nodedeps/_workspace
-cd nodepath/src/github.com/UmamaheshMaxwell/node-basic/
+cd nodepath/github.com/UmamaheshMaxwell/node-basic/
 
 npm run test
