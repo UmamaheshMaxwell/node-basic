469deb60587ca0b87e28d1a4ef74da2b259b43bb
simhachalam <simha@olivetech.net>
2019-02-14 14:50:07 +0530

    deploy to ecr

--- test.sh
+++ test.sh
@@ -3,12 +3,16 @@
 set -eu
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export fly_target=${fly_target:-tutorial}
+export fly_target=${fly_target:-cons}
 echo "Concourse API target ${fly_target}"
 echo "Tutorial $(basename $DIR)"
+export branchName=$(git branch | grep \* | cut -d ' ' -f2)
+cat > ./ci/feature-version.yml << EOF
+branch-version: ${branchName}
+EOF
 
 pushd "$DIR"
-  fly -t ${fly_target} set-pipeline -p node-pipeline -c ci/pipeline.yml -n
+  fly -t ${fly_target} set-pipeline -p node-pipeline -c ci/pipeline.yml  --load-vars-from=ci/feature-version.yml -n
   fly -t ${fly_target} unpause-pipeline -p node-pipeline
   fly -t ${fly_target} trigger-job -w -j node-pipeline/job-run-master
   fly -t ${fly_target} trigger-job -w -j node-pipeline/job-run-develop
